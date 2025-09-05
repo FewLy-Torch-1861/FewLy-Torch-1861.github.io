28818d3e1f231da486d1e11e7e7b125ecf49df29
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-09-05 17:13:04 +0700

    center the clock

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function updateClock() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    clockElement.textContent = `${hours}:${minutes}`;
+    clockElement.textContent = `it's ${hours}:${minutes} now!`;
   }
 }
 
